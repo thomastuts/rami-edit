8a24d5059edc7cbf60997dab8573bb34946da335
Thomas Tuts <thomas.tuts@gmail.com>
2015-08-06 22:27:22 +0200

    Add styling for toolbar icon tray

--- app/src/components/toolbar.js
+++ app/src/components/toolbar.js
@@ -14,6 +14,20 @@ export default class Toolbar extends React.Component {
           <li className="toolbar__system-button__orb toolbar__system-button__orb--maximize"></li>
         </ul>
         <div className="toolbar__window-title">{this.props.title}</div>
+        <ul className="toolbar__icon-tray">
+          <li className="active">
+            <img src="img/icon-pen.svg" className="toolbar__icon-tray__icon" />
+            <div className="toolbar__icon-tray__title">Edit</div>
+          </li>
+          <li>
+            <img src="img/icon-books.svg" className="toolbar__icon-tray__icon" />
+            <div className="toolbar__icon-tray__title">Library</div>
+          </li>
+          <li>
+            <img src="img/icon-slideshow.svg" className="toolbar__icon-tray__icon" />
+            <div className="toolbar__icon-tray__title">Slideshow</div>
+          </li>
+        </ul>
       </div>
     );
   }
