83cd9d7a2d8984c1f1871c3d8618f2d1c6faf700
Thomas Tuts <thomas.tuts@gmail.com>
2015-08-07 08:53:51 +0200

    Add basic toggler for sidebar sections

--- app/src/components/sidebar/sidebar-section.js
+++ app/src/components/sidebar/sidebar-section.js
@@ -1,20 +1,55 @@
 import React from 'react';
+import $ from 'jquery';
 
 export default class SidebarSection extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      height: null,
+      actualHeight: 0,
+      isExpanded: true
+    };
   }
 
+  componentDidMount() {
+    setTimeout(() => {
+      let sectionHeight = $(this.refs.section.getDOMNode()).height();
+      console.log(sectionHeight);
+      this.setState({
+        actualHeight: sectionHeight,
+        height: sectionHeight
+      });
+    }, 100);
+  }
+
+  toggle = () => {
+    console.log('Toggling');
+    this.setState({
+      height: this.state.isExpanded ? 40 : this.state.actualHeight,
+      isExpanded: !this.state.isExpanded
+    });
+  };
+
   render() {
+    let sectionStyle = {
+      height: this.state.height + 'px'
+    };
+
+    let contentStyle = {
+      visibility: this.state.isExpanded ? 'visible' : 'hidden',
+      opacity: this.state.isExpanded ? 1 : 0
+    };
+
     return (
-      <div className="sidebar__content__section">
-        <div className="sidebar__content__section__header">
+      <div className="sidebar__content__section" style={sectionStyle} ref="section">
+        <div className="sidebar__content__section__header" onClick={this.toggle}>
           {this.props.title}
           <div className="sidebar__content__section__header__toggle-icon">
             <img src="img/angle-down.svg" />
           </div>
         </div>
-        <div className="sidebar__content__section__content">{this.props.children}</div>
+        <div className="sidebar__content__section__content" style={contentStyle}>{this.props.children}</div>
       </div>
     );
   }
