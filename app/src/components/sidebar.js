0bbc3ca9e99256b53e14cbb9e12078a2080e169e
Thomas Tuts <thomas.tuts@gmail.com>
2015-08-07 07:08:52 +0200

    Implement sidebar layout and visibility

--- app/src/components/sidebar.js
+++ app/src/components/sidebar.js
@@ -3,11 +3,21 @@ import React from 'react';
 export default class Sidebar extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      isVisible: true
+    };
   }
 
   render() {
+    let sidebarClasses = ['sidebar'];
+
+    if (this.state.isVisible) {
+      sidebarClasses.push('sidebar--visible');
+    }
+
     return (
-      <div className="sidebar">
+      <div className={sidebarClasses.join(' ')}>
         <div className="sidebar__close-anchor"></div>
         <div className="sidebar__content"></div>
       </div>
